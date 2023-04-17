172b529f7734a9c4590052057f7e18b201fb40c2
Njål Telstø <njal_telsto@hotmail.com>
2023-04-17 18:39:58 +0200

    Changed default image

--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,7 @@ int main()
             "assets/shaders/render.frag");
     assert(render_shader.valid());
 
-    SlimeSimulator simulator(1000000, "assets/images/leonid2.jpg");
+    SlimeSimulator simulator(1000000, "assets/images/leonid.jpg");
 
     glm::ivec2 size = simulator.map_size();
     Graphics::set_aspect(size.x, size.y);
